402a08a9881d76831b4918a7b31a7a0ce3180cbe
daditsan <100355396+daditsan@users.noreply.github.com>
2024-06-20 02:20:55 +0700

    - nambahin section kiri dan kanan, jadi total ada 3 section: section kiri, section tengah (board), section - layout: ngerubah warna layout, untuk memudahkan ngeliat batas batas per section nya - section kiri & kanan: ini ntar bakal jadi tempat penampung untuk fitur chat - button: ngerubah bentuk button, efek hover,
    known bugs:
    - belum responsive, kalo dimension viewnya pake view mobile/tablet layout pasti jadi kacau. ga sempat ngaturnya
    - ada angka 0 kalo tombol untuk munculin board (button board), belum di trigger

--- src/App.jsx
+++ src/App.jsx
@@ -40,26 +40,83 @@ function App() {
 
   return (
     <>
-      <p>{board}</p>
-      <button onClick={tes}>P firu</button>
-      <button onClick={maen}>Maen</button>
-      {board.length &&
-        board.map((row) => {
-          return (
-            <div style={{ display: "flex" }}>
-              {row.map((col) => (
-                <div style={{ backgroundColor: "green", border: "1px solid black", width: 50, height: 50, padding: 5 }}>
-                  <span>{col}</span>
-                  {players
-                    .filter((p) => p.position == col)
-                    .map((p) => (
-                      <span style={{ color: p.color }}>O</span>
+      <div>
+        {/* <p>{board}</p> */}
+        <div className="App bg-blue-900 h-screen flex justify-center items-center vh-100">
+          <div
+            style={{
+              backgroundColor: "#E1EFFE",
+              height: "100vh",
+              width: "30vw",
+              marginRight: "100px",
+              color: "black",
+            }}
+          >
+            section kiri, di section ini atau section kanan bakal di taro chat,
+            atau hiasan lain
+          </div>
+          <div className="flex flex-col items-center">
+            {board.length &&
+              board.map((row) => {
+                return (
+                  // <div style={{ display: "flex" }}>
+                  <div className="grid grid-cols-10">
+                    {row.map((col) => (
+                      // <div
+                      //   style={{
+                      //     backgroundColor: "green",
+                      //     border: "1px solid black",
+                      //     width: 50,
+                      //     height: 50,
+                      //     padding: 5,
+                      //   }}
+                      // >
+                      <div className="cell bg-blue-700 border flex items-center justify-center h-12 w-12 relative">
+                        <span>{col}</span>
+                        {players
+                          .filter((p) => p.position == col)
+                          .map((p) => (
+                            <span style={{ color: p.color }}>0</span>
+                          ))}
+                      </div>
                     ))}
-                </div>
-              ))}
+                  </div>
+                );
+              })}
+            <div className="flex w-full">
+              <ul className="w-full">
+                <li>
+                  <button
+                    onClick={tes}
+                    className="mt-4 w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Board
+                  </button>
+                </li>
+                <li>
+                  <button
+                    onClick={maen}
+                    className="mt-4 w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Roll Dice
+                  </button>
+                </li>
+              </ul>
             </div>
-          );
-        })}
+          </div>
+          <div
+            style={{
+              backgroundColor: "#E1EFFE",
+              height: "100vh",
+              width: "30vw",
+              marginLeft: "100px",
+              color: "black",
+            }}
+          >
+            section kanan
+          </div>
+        </div>
+      </div>
     </>
   );
 }
